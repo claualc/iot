2f5646a3377f06ade2911d069b7d39ea401c4f21
claualc <claudia.baz.alvarez@gmail.com>
2023-05-25 13:09:10 +0200

    add: payload types

--- Challenge/RadioRoute-Template/RadioRoute.h
+++ Challenge/RadioRoute-Template/RadioRoute.h
@@ -4,12 +4,24 @@
 #define RADIO_ROUTE_H
 
 typedef nx_struct radio_route_msg {
-	//field 1
-	//field 2
-	//field 3
-	//....
-	//field n
-	nx_uint16_t counter;
+	/*
+	type: defines the message format
+		0 - DATA MSG
+		1 - ROUTE_REQ MSG
+		2 - ROUTE_REPLY
+	*/
+	nx_int8_t type;
+	nx_int8_t src;  // Sender
+	nx_int8_t dest; // Node Requested
+	/*
+	value: defines msg payload
+	The meaning of the value attr changes 
+	with the type of the msg:
+		if DATA MSG  - led value
+		if ROUTE_REQ - none
+		if REPLY_REQ - hop_count
+	*/
+	nx_int8_t value;
 } radio_route_msg_t;
 
 enum {
