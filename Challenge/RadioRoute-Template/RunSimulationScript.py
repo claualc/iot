75281fb55563edae5d75eaa650e4368dbbb8c8d5
claualc <claudia.baz.alvarez@gmail.com>
2023-05-27 16:50:34 +0200

    fix: segmentation fault

--- Challenge/RadioRoute-Template/RunSimulationScript.py
+++ Challenge/RadioRoute-Template/RunSimulationScript.py
@@ -66,40 +66,39 @@ print ">>>Will boot at time",  time1/t.ticksPerSecond(), "[sec]";
 
 print "Creating node 2...";
 node2 = t.getNode(2);
-time2 = 1*t.ticksPerSecond();
+time2 = 0*t.ticksPerSecond();
 node2.bootAtTime(time2);
 print ">>>Will boot at time", time2/t.ticksPerSecond(), "[sec]";
 
 print "Creating node 3...";
 node3 = t.getNode(3);
-time3 = 1*t.ticksPerSecond();
+time3 = 0*t.ticksPerSecond();
 node3.bootAtTime(time3);
 print ">>>Will boot at time", time3/t.ticksPerSecond(), "[sec]";
 
 print "Creating node 4...";
 node4 = t.getNode(4);
-time4 = 1*t.ticksPerSecond();
+time4 = 0*t.ticksPerSecond();
 node4.bootAtTime(time4);
 print ">>>Will boot at time", time4/t.ticksPerSecond(), "[sec]";
 
 print "Creating node 5...";
 node5 = t.getNode(5);
-time5 = 1*t.ticksPerSecond();
+time5 = 0*t.ticksPerSecond();
 node5.bootAtTime(time5);
 print ">>>Will boot at time", time5/t.ticksPerSecond(), "[sec]";
 
 print "Creating node 6...";
 node6 = t.getNode(6);
-time6 = 1*t.ticksPerSecond();
+time6 = 0*t.ticksPerSecond();
 node6.bootAtTime(time6);
 print ">>>Will boot at time", time6/t.ticksPerSecond(), "[sec]";
 
 print "Creating node 7...";
 node7 = t.getNode(7);
-time7 = 1*t.ticksPerSecond();
+time7 = 0*t.ticksPerSecond();
 node7.bootAtTime(time7);
 print ">>>Will boot at time", time7/t.ticksPerSecond(), "[sec]";
-
 
 
 print "Creating radio channels..."
@@ -108,7 +107,7 @@ lines = f.readlines()
 for line in lines:
   s = line.split()
   if (len(s) > 0):
-    print ">>>Setting radio channel from node ", s[0], " to node ", s[1], " with gain ", s[7], " dBm"
+    print ">>>Setting radio channel from node ", s[0], " to node ", s[1], " with gain ", s[2], " dBm"
     radio.add(int(s[0]), int(s[1]), float(s[2]))
 
 
@@ -145,4 +144,3 @@ for i in range(0,1200):
 	t.runNextEvent()
 	
 print "\n\n\nSimulation finished!";
-
